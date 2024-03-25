7280350112bc4a027f78041a4e635a55461cba32
João Pedro Lopes <joaopalopes24@gmail.com>
2024-03-25 17:57:17 -0300

    test(tests): adding tests and gitub workflow

---
+++ tests/LoginStatelessTest.php
@@ -0,0 +1,75 @@
+<?php
+
+use Livewire\Volt\Volt;
+use Tests\TestCase;
+use Tests\User;
+
+uses(TestCase::class);
+
+test('it should render the login screen', function () {
+    $response = $this->get('/login');
+
+    $response->assertOk()->assertViewIs('login-stateless::page');
+});
+
+test('it should authenticate the master user', function () {
+    $user = User::factory()->create(['email' => $this->master]);
+
+    $component = Volt::test('login')
+        ->set('form.email', $user->email)
+        ->set('form.password', 'password');
+
+    $component->call('login');
+
+    $this->assertAuthenticatedAs($user);
+
+    $component->assertHasNoErrors()->assertRedirect('/dashboard');
+});
+
+test('it should block unauthorized users', function () {
+    $user = User::factory()->create();
+
+    $component = Volt::test('login')
+        ->set('form.email', $user->email)
+        ->set('form.password', 'password');
+
+    $component->call('login');
+
+    $this->assertGuest();
+
+    $component->assertHasErrors(['form.email' => [trans('auth.failed')]]);
+});
+
+test('it should render the dashboard for admin users', function () {
+    $user = User::factory()->create(['email' => $this->master]);
+
+    $this->actingAs($user);
+
+    $response = $this->get('/dashboard');
+
+    $response->assertOk()->assertViewIs('login-stateless::page');
+});
+
+test('it should block unauthorized users from accessing the dashboard', function () {
+    $user = User::factory()->create();
+
+    $this->actingAs($user);
+
+    $response = $this->get('/dashboard');
+
+    $response->assertForbidden();
+});
+
+test('it should logout the authenticated user', function () {
+    $user = User::factory()->create();
+
+    $this->actingAs($user);
+
+    $component = Volt::test('dashboard');
+
+    $component->call('logout');
+
+    $this->assertGuest();
+
+    $component->assertHasNoErrors()->assertRedirect('/');
+});
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,35 @@
 
 namespace Tests;
 
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Gate;
+use Livewire\LivewireServiceProvider;
+use Livewire\Volt\VoltServiceProvider;
 use LoginStateless\ServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use WireUi\Heroicons\HeroiconsServiceProvider;
+use WireUi\ServiceProvider as WireUiServiceProvider;
 
 abstract class TestCase extends OrchestraTestCase
 {
+    use RefreshDatabase;
+
+    /**
+     * Master user email.
+     */
+    public string $master = 'test@example.com';
+
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Gate::define('admin', fn ($user = null) => $user && $user->email === $this->master);
+    }
+
     /**
      * Load package service provider
      */
@@ -14,6 +38,28 @@ abstract class TestCase extends OrchestraTestCase
     {
         return [
             ServiceProvider::class,
+            VoltServiceProvider::class,
+            WireUiServiceProvider::class,
+            LivewireServiceProvider::class,
+            HeroiconsServiceProvider::class,
         ];
+    }
+
+    /**
+     * Define database migrations.
+     */
+    protected function defineDatabaseMigrations(): void
+    {
+        $this->loadLaravelMigrations();
+    }
+
+    /**
+     * Define environment setup.
+     */
+    protected function defineEnvironment($app): void
+    {
+        tap($app['config'], function (Repository $config) {
+            $config->set('system.admin', $this->master);
+        });
     }
 }
